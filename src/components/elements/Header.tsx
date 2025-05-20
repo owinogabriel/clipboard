1a9a92f53cc377d51c1a00c832ff17e67f6547bd
Leon Gabriel <g73346920@gmail.com>
2025-05-20 18:09:08 +0300

    uodate:Modified

--- src/components/elements/Header.tsx
+++ src/components/elements/Header.tsx
@@ -3,23 +3,20 @@ import logo from "/assets/logo.svg"
 
 const Header = () => {
   return (
-    <header className="flex flex-col bg-[url('assets/bg-header-desktop.png')] bg-cover bg-no-repeat w-full mb-6">
-      <div className="flex flex-col items-center p-15">
-        <img src={logo} alt='Logo' className='w-20 h-20' />
-        <h1 className="mt-4 font-sans ">A history of everything you copy</h1>
-        <p className="max-w-[60ch] py-2 font-sans">Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.</p>
+<header className="flex flex-col bg-[url('assets/bg-header-desktop.png')] bg-cover bg-no-repeat w-full mb-6">
+  <div className="flex flex-col items-center px-4 py-10 text-center">
+    <img alt="Logo" className="w-20 h-20" src={logo} />
+    <h1 className="mt-4 font-sans text-2xl">A history of everything you copy</h1>
+    <p className="max-w-[60ch] py-2 font-sans text-gray-600">
+      Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.
+    </p>
+    <div className="flex flex-col sm:flex-row mt-4 gap-4">
+      <Button className="px-8 py-3 rounded-full text-white bg-green-600 hover:bg-green-500">Download for iOS</Button>
+      <Button className="px-8 py-3 rounded-full text-white bg-violet-500 hover:bg-violet-400">Download for Mac</Button>
+    </div>
+  </div>
+</header>
 
-       <div className="flex mt-4 gap-x-4">
-       <Button className="bg-green-600 hover:bg-green-300">
-          <span>Downkoad for iOS</span>
-        </Button>
-        <Button className="bg-violet-500 hover:bg-violet-300">
-          <span>Downkoad for Mac</span>
-        </Button>
-        </div>
-      </div>
-
-    </header>
   )
 }
 
