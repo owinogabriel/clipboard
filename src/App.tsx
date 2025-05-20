eb9df6ba3de5a41a49a476d7eb24dde111796772
Leon Gabriel <g73346920@gmail.com>
2025-05-20 18:09:58 +0300

    Chored:section files imported

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import { Layout } from './components/Layout'
+import SnippetsTrack from './components/sections/SnippetsTrack'
+import ClipboardAccess from './components/sections/ClipboardAccess'
+import {Brand, Gallery} from './components/sections/Gallery'
 
 function App() {
   const [] = useState(0)
 
   return (
-  <Layout title="Board">
-<div>
-  
-</div>
-  </Layout>
+    <Layout title="ClipBoard | Landing Page">
+      <SnippetsTrack />
+      <ClipboardAccess />
+      <Gallery />
+      <Brand />
+    </Layout>
   )
 }
 
