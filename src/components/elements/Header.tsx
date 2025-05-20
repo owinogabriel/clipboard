2174cea7463ba2df22486eb12076930ad1e37fdf
Leon Gabriel <g73346920@gmail.com>
2025-05-20 17:23:31 +0300

    modified

--- src/components/elements/Header.tsx
+++ src/components/elements/Header.tsx
@@ -3,11 +3,11 @@ import logo from "/assets/logo.svg"
 
 const Header = () => {
   return (
-    <header className="flex flex-col bg-[url('assets/bg-header-desktop.png')] h-70 bg-cover bg-no-repeat w-full">
+    <header className="flex flex-col bg-[url('assets/bg-header-desktop.png')] bg-cover bg-no-repeat w-full mb-6">
       <div className="flex flex-col items-center p-15">
         <img src={logo} alt='Logo' className='w-20 h-20' />
-        <h1 className="mt-4 font-semibold font-sans text-gray-700 text-2xl">A history of everything you copy</h1>
-        <p className="max-w-[60ch] py-2 text-gray-500 font-sans">Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.</p>
+        <h1 className="mt-4 font-sans ">A history of everything you copy</h1>
+        <p className="max-w-[60ch] py-2 font-sans">Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.</p>
 
        <div className="flex mt-4 gap-x-4">
        <Button className="bg-green-600 hover:bg-green-300">
