69a9ff6b77f42d84cb2384200cec81070fde5f13
Leon Gabriel <g73346920@gmail.com>
2025-05-21 12:53:25 +0300

    update:layout set to responsive when in both screen devices

--- src/components/sections/SnippetsTrack.tsx
+++ src/components/sections/SnippetsTrack.tsx
@@ -25,7 +25,7 @@ const SnippetsTrack = () => {
       })
       .then(data => {
         if (Array.isArray(data)) {
-        const itemsWithUniqueIds = data.map(item => ({
+          const itemsWithUniqueIds = data.map(item => ({
             ...item,
             id: uuidv4() // Assigns a unique ID using uuid
           }));
@@ -51,30 +51,30 @@ const SnippetsTrack = () => {
   return (
     <div className="flex  flex-col mt-10 items-center">
       <h1 className="mt-6">Keep track of your snippets</h1>
-      <p className="max-w-[70ch] mt-5 mb-10">Clipboard instantly stores any item you copy in the cloud, meaning you can access
+      <p className="max-w-[35ch] lg:max-w-[70ch] mt-5 mb-10 text-center">Clipboard instantly stores any item you copy in the cloud, meaning you can access
         your snippets immediately on all your devices. Our Mac and Ios apps will help you organize everything.
       </p>
 
       <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-items-center gap-8 p-6">
-  {/* Image Section */}
-  <div className="flex justify-center">
-    <img src={Mac} alt="Mac Image" className="max-w-full h-auto rounded" />
-  </div>
+        {/* Image Section */}
+        <div className="flex justify-center">
+          <img src={Mac} alt="Mac Image" className="max-w-full h-auto rounded" />
+        </div>
 
-  {/* Items Section */}
-  {items.length > 0 ? (
-    <ul className="space-y-6 text-center md:text-left">
-      {items.map(item => (
-        <li key={item.id} className="p-2">
-          <h3 className="text-2xl text-gray-700 font-semibold">{item.title}</h3>
-          <p className="max-w-[40ch] text-gray-500 mt-2 mx-auto md:mx-0">{item.Description}</p>
-        </li>
-      ))}
-    </ul>
-  ) : (
-    <p className="text-center text-gray-500">No features to display.</p>
-  )}
-</div>
+        {/* Items Section */}
+        {items.length > 0 ? (
+          <ul className="space-y-6  md:text-left">
+            {items.map(item => (
+              <li key={item.id} className="p-2 text-justify">
+                <h3 className="text-2xl text-gray-700 font-semibold">{item.title}</h3>
+                <p className="max-w-[40ch] text-gray-500 mt-2 mx-auto md:mx-0">{item.Description}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-center text-gray-500">No features to display.</p>
+        )}
+      </div>
 
     </div>
   )
