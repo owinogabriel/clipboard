d47fd8aa4560c3bbdcfcfd6cbbd0ec280b906a67
Leon Gabriel <g73346920@gmail.com>
2025-05-21 12:52:50 +0300

    chored:changed export to default since they is no other file exported.

--- src/components/shared/ImageCard.tsx
+++ src/components/shared/ImageCard.tsx
@@ -5,15 +5,14 @@ interface ImageProps {
   description: string
 }
 
-
-
-
-export const ImageCard = ({ image, title, description }: ImageProps) => (
+const ImageCard = ({ image, title, description }: ImageProps) => (
   <div className="flex flex-col  items-center transition-transform hover:scale-105">
     <img src={image} alt={title} className="w-10 h-10 " />
-    <div className="p-4  flex flex-col items-center">
+    <div className="p-4 ">
       <h2 className="text-lg font-semibold mb-1">{title}</h2>
-      <p className="text-gray-600 text-base max-w-[40ch]">{description}</p>
+      <p className="text-gray-600 max-w-[35ch]">{description}</p>
     </div>
   </div>
 );
+
+export default ImageCard;
